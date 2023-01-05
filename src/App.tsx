6fa9f6fd1c783860b8f3979877ac3905a708e6f3
mazurenka <an.drej@mail.ru>
2023-01-05 12:42:15 +0100

    04

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,19 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
-import { v1 } from 'uuid';
+import {v1} from 'uuid';
 
 export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
     let [tasks, setTasks] = useState([
-        { id: v1(), title: "HTML&CSS", isDone: true },
-        { id: v1(), title: "JS", isDone: true },
-        { id: v1(), title: "ReactJS", isDone: false },
-        { id: v1(), title: "Rest API", isDone: false },
-        { id: v1(), title: "GraphQL", isDone: false },
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "ReactJS", isDone: false},
+        {id: v1(), title: "Rest API", isDone: false},
+        {id: v1(), title: "GraphQL", isDone: false},
     ]);
-
-    function removeTask(id: string) {
-        let filteredTasks = tasks.filter(t => t.id != id);
-        setTasks(filteredTasks);
-    }
-
-    function addTask(title: string) {
-        let task = { id: v1(), title: title, isDone: false };
-        let newTasks = [task, ...tasks];
-        setTasks(newTasks);
-    }
 
     let [filter, setFilter] = useState<FilterValuesType>("all");
 
@@ -37,11 +26,29 @@ function App() {
         tasksForTodolist = tasks.filter(t => t.isDone === true);
     }
 
+    function removeTask(id: string) {
+        let filteredTasks = tasks.filter(t => t.id != id);
+        setTasks(filteredTasks);
+    }
+
+    function addTask(title: string) {
+        let task = {id: v1(), title: title, isDone: false};
+        let newTasks = [task, ...tasks];
+        setTasks(newTasks);
+    }
+
     function changeFilter(value: FilterValuesType) {
         setFilter(value);
     }
 
-
+    const changeStatus = (id: string) => {
+       /* const taskForUpdate = tasks.find(t => t.id === id)
+        if(taskForUpdate){
+            taskForUpdate.isDone = !taskForUpdate.isDone
+            setTasks([...tasks])
+        }*/
+        setTasks(tasks.map(t => t.id === id ? {...t, isDone: !t.isDone} : t))
+    }
 
     return (
         <div className="App">
@@ -49,7 +56,8 @@ function App() {
                       tasks={tasksForTodolist}
                       removeTask={removeTask}
                       changeFilter={changeFilter}
-                      addTask={addTask} />
+                      addTask={addTask}
+                      changeStatus={changeStatus}/>
         </div>
     );
 }
